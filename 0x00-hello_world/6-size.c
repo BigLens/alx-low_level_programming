b4c31ff117cba36d51455a4372e608675f3e8f0a
BigLens <adedejibioku@gmail.com>
2023-06-18 09:39:02 -0700

    outputing function with puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n");
+	printf("Size of an int: %d byte(s)\n");
+	printf("Size of a long int: %d byte(s)\n");
+	printf("Size of long long int: %d byte(s)\n");
+	printf("Size of a float: %d byte(s)\n");
+	return (0);
+}
