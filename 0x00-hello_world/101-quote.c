8d58e43b9b769f220fb5d49f845023effc28f125
BigLens <adedejibioku@gmail.com>
2023-06-18 12:18:24 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	return (1);
+}
