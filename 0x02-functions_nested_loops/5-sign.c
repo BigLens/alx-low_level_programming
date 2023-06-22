029e96d5bd762ed181829d87605ee66b0adc039a
BigLens <adedejibioku@gmail.com>
2023-06-21 21:23:34 -0700

    sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry
+ *
+ * Return: suceess
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
