0dfa8e0f51baf174bcebd772cc507e1179d3a29a
BigLens <adedejibioku@gmail.com>
2023-06-21 21:34:33 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * _abs - computes the absolute value of an integrer
- *
- *
- * Return: the absolute value of int
- *
+ * _abs - compute the absolute value of an integer
+ * @n: int type number
+ * Return: absolute value of @n
  */
 int _abs(int n)
 {
-	if (n >= 0)
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
 	{
 		return (n);
 	}
-	return (-n);
 }
