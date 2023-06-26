bb6213ec3188673fc78780c088d4bc50ffd0d2df
BigLens <adedejibioku@gmail.com>
2023-06-25 18:48:15 -0700

    printing numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - printing numbers from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		_putchar(b);
+	}
+
+	_putchar('\n');
+}
