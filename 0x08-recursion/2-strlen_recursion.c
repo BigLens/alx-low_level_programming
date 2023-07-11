aa24cc1cb4b66fbbe6aea2da093446985590eac3
BigLens <adedejibioku@gmail.com>
2023-07-11 06:45:28 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - get length of string
+ * @s: pointer to a string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
